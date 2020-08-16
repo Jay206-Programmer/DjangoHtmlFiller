6ed9dbdfc4661c17ae931da7d85d342649b4a6f8
Jay Shukla <jayshukla0034@gmail.com>
2020-08-16 20:04:36 +0530

    Added Setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="DjangoHtmlFiller",
+    version="1.0.0",
+    description="Auto html filler for Django Project's html files.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jay206-Programmer/DjangoHtmlFiller",
+    author="Jay Shukla",
+    author_email="jayshukla0034@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8.2",
+    ],
+    packages=["DjangoHtmlFiller"],
+    include_package_data=True,
+    install_requires=["os","sys"],
+)
